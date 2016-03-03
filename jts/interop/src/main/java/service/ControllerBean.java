02784db557341c3d5f5c0bb795903dcd76b667ea
Michael Musgrove <mmusgrov@redhat.com>
2016-03-03 19:32:57 +0000

    nearly there

--- jts/interop/src/main/java/service/ControllerBean.java
+++ jts/interop/src/main/java/service/ControllerBean.java
@@ -11,9 +11,7 @@ import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
-import java.rmi.RemoteException;
 import java.util.Properties;
 
 @TransactionManagement(TransactionManagementType.CONTAINER)
@@ -44,107 +42,56 @@ public class ControllerBean {
         }
     }
 
-    private ISession xxxgetServiceEJB(int jndiPort, String jndiName, String facClass, String providerUrl) throws RemoteException, NamingException {
-        Properties env = new Properties();
-
-        if (providerUrl != null) {
-            env.put(Context.PROVIDER_URL, providerUrl);
-        }
-        env.put(Context.INITIAL_CONTEXT_FACTORY, facClass);
-        env.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
-        env.setProperty("org.omg.CORBA.ORBInitialPort", String.valueOf(jndiPort));
-
-//        name = "corbaname:iiop:localhost:3700#java:global/ejbtest/SessionBean"; //"java:global/ejbtest/SessionBean"
-
-        System.out.printf("looking up %s via port %d%n", jndiName, jndiPort);
-
-        Object oRef = new InitialContext(env).lookup(jndiName);
-
-        System.out.printf("look up returned %s%n", oRef);
-
-        ISessionHome home = (ISessionHome) oRef; //PortableRemoteObject.narrow(oRef, ISessionHome.class);
-
-        return home.create();
-    }
-
-    private ISession getServiceEJB(int jndiPort, String jndiName, String facClass, String providerUrl) throws RemoteException, NamingException {
-        Properties env = new Properties();
-
-        if (providerUrl != null)
-            env.put(Context.PROVIDER_URL, providerUrl);
-
-        env.put(Context.INITIAL_CONTEXT_FACTORY, facClass);
-        //env.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
-        //env.setProperty("org.omg.CORBA.ORBInitialPort", String.valueOf(jndiPort));
-
-        System.out.printf("looking up %s via port %d%n", jndiName, jndiPort);
-
-        Object oRef = new InitialContext(env).lookup(jndiName);
-
-        System.out.printf("look up returned %s%n", oRef);
-
-        ISessionHome home = (ISessionHome) oRef; //PortableRemoteObject.narrow(oRef, ISessionHome.class);
-
-        return home.create();
-    }
-
     private ISession getServiceEJB(boolean local, String as, int jndiPort) {
         // "java:global/[<application-name>]/<module-name>/<bean-name>!<fully-qualified-bean-interface-name>"
         String name = "java:global/ejbtest/SessionBean!service.remote.ISessionHome";
-        Context ctx;
+        Properties env = new Properties();
 
-        if (local) {
-            try {
-                ctx = new InitialContext();
-                Object obj = ctx.lookup(name);
-                ISessionHome home = (ISessionHome) obj; // PortableRemoteObject.narrow(obj, ISessionHome.class);
-
-                return home.create();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Properties env = new Properties();
-            String providerUrl;
-
+        if (!local) {
             if (isWF) {
                 // running on a WildFly server
-                providerUrl = "corbaloc::localhost:%d/NameService";
 
                 if (jndiPort <= 0)
                     jndiPort = 7001; // corba name service port for glassfish domain1
 
-
-                try {
-                    return getServiceEJB(jndiPort, name, "org.wildfly.iiop.openjdk.naming.jndi.CNCtxFactory", String.format(providerUrl, jndiPort));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
+                env.put(Context.PROVIDER_URL, String.format("corbaloc::localhost:%d/NameService", jndiPort));
+                env.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.iiop.openjdk.naming.jndi.CNCtxFactory");
             } else {
                 // running on a glassfish server
                 String facClass; // set context factory depending upon whether we are looking up via glassfish or wildfly
 
                 if ("gf".equals(as)) { // lookup an ejb running on a glassfish server
-                    facClass = "com.sun.enterprise.naming.SerialInitContextFactory";
-                    providerUrl = null;
 
                     if (jndiPort <= 0)
                         jndiPort = 3700; // corba name service port for glassfish domain2
+
+                    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+                    env.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+                    env.setProperty("org.omg.CORBA.ORBInitialPort", String.valueOf(jndiPort));
+
                 } else {  // lookup an ejb running on a wildfly server
-                    facClass = "com.sun.jndi.cosnaming.CNCtxFactory";
-                    providerUrl = "iiop://localhost:3528";
                     name = "ejbtest/SessionBean";
 
                     if (jndiPort <= 0)
                         jndiPort = 3528; // default corba name service port for wildfly
-                }
 
-                try {
-                    return getServiceEJB(jndiPort, name, facClass, providerUrl);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    env.put(Context.PROVIDER_URL, String.format("iiop://localhost:%d", jndiPort));
+                    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.cosnaming.CNCtxFactory");
                 }
+            }
+
+            try {
+                System.out.printf("looking up %s via port %d%n", name, jndiPort);
+
+                Object oRef = new InitialContext(env).lookup(name);
+
+                System.out.printf("look up returned %s%n", oRef);
+
+                ISessionHome home = (ISessionHome) oRef; //PortableRemoteObject.narrow(oRef, ISessionHome.class);
+
+                return home.create();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
 
